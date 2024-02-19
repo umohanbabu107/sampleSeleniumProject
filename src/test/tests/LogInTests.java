61275030862b93d3125158bd7a5c586b5cfeb499
Mohan <umohanbabu107@gmail.com>
2024-02-19 19:07:10 +0530

    Project creation

---
+++ src/test/tests/LogInTests.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LogInTests {
+
+    @Test
+    public void test1(){
+        ChromeDriver driver = new ChromeDriver();
+    }
+
+}
