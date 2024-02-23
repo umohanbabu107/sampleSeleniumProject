91b7d5acd02584cdc6bc518fc22398b7757dc78e
Mohan <umohanbabu107@gmail.com>
2024-02-23 14:21:16 +0530

    Login tests fix

--- src/test/tests/LogInTests.java
+++ src/test/tests/LogInTests.java
@@ -2,9 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pages.OrangeHRMLoginPage;
 
